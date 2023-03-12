7e490bf302db73adb453ec65ea7a7a3d71bcd486
hyebeen byun <hyebeen2658@naver.com>
2023-03-12 22:32:26 +0900

    [setting] 공통 스타일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/common/index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
