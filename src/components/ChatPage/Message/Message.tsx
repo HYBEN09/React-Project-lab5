d23456aabadb4effc8654b47dafe16932aa8812d
hyebeen byun <hyebeen2658@naver.com>
2023-03-13 10:00:25 +0900

    [style] Message component 마크업&스타일

---
+++ src/components/ChatPage/Message/Message.tsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import classes from "./Message.module.scss";
+export const Message = () => {
+  // const combineClassNames = `${classes.message} ${classes.owner}`.trim();
+
+  return (
+    <div className={classNames(classes.message, classes.owner)}>
+      <div className={classes.messageInfo}>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt=""
+        />
+        <span>지금</span>
+      </div>
+      <div className={classes.messageContent}>
+        <p>안녕하세요☺️</p>
+        <img
+          src="https://avatars.githubusercontent.com/u/38703262?v=4"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
